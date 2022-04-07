d44bf52d23286d988684ee7d15064096d993a7c3
AdrianValladares <a.valladares.d@gmail.com>
2022-04-07 09:36:13 +0200

    initial commit

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from "next/document"
+
+export default function MyDocument(props: any) {
+  return (
+    <Html>
+      <Head></Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
