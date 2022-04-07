fefacb0bccea8cfc0eb1b35d84392b240d689b3b
AdrianValladares <a.valladares.d@gmail.com>
2022-04-07 12:02:39 +0200

    fixed favicon route

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,10 @@ export default function MyDocument(props: any) {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Unique things." />
+        <link rel="icon" href="/images/favicon.ico" />
       </Head>
       <body>
         <Main />
