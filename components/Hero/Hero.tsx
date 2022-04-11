7f1c95770a0f34abbdf99a4c77a125271444330b
AdrianValladares <a.valladares.d@gmail.com>
2022-04-11 13:16:56 +0200

    implemented images as content

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,17 +1,24 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 interface HeroProps {
-  content: any[];
+  content: ReactNode[];
 }
 
 const Hero = ({ content }: HeroProps) => {
   const [index, setIndex] = useState(0);
+  const centerRef = useRef<HTMLDivElement>(null);
+  const leftRef = useRef<HTMLDivElement>(null);
+  const rightRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {}, [index]);
 
   const moveLeft = () => {
-    setIndex((index + content.length - 1) % content.length);
+    leftRef.current?.classList.add("hero-content__left--centered");
+    setTimeout(() => {
+      leftRef.current?.classList.remove("hero-content__left--centered");
+      setIndex((index + content.length - 1) % content.length);
+    }, 1500);
   };
 
   const moveRight = () => {
@@ -27,11 +34,13 @@ const Hero = ({ content }: HeroProps) => {
   return (
     <StyledHero className="hero">
       <div className="hero-content">
-        <div className="hero-content__center">{content[index]}</div>
-        <div className="hero-content__left">
+        <div className="hero-content__center" ref={centerRef}>
+          {content[index]}
+        </div>
+        <div className="hero-content__left" ref={leftRef}>
           {content[findNextContentIndex("left")]}
         </div>
-        <div className="hero-content__right">
+        <div className="hero-content__right" ref={rightRef}>
           {content[findNextContentIndex("right")]}
         </div>
       </div>
@@ -53,11 +62,25 @@ const StyledHero = styled.article`
     height: 100%;
     overflow: hidden;
     display: flex;
+    position: relative;
     & > * {
       width: 100%;
       height: 100%;
       flex: 1 0 auto;
       border: 2px solid black;
+      position: absolute;
+      &.hero-content {
+        &__left {
+          transform: translateX(-100%);
+          &--centered {
+            transition: all 1s linear;
+            transform: translateX(0);
+          }
+        }
+        &__right {
+          transform: translateX(100%);
+        }
+      }
     }
   }
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import Hero from "../components/Hero/Hero";
+import landscape from "/public/images/colorfulLandscape.webp";
+import stairs from "/public/images/escaleras.jpg";
+import oceanWaves from "/public/images/oceanWaves.jpg";
+import plantitas from "/public/images/plantitas.jpg";
+import purpleWave from "/public/images/purpleWave.jpg";
+import smoothWaves from "/public/images/smoothWaves.jpg";
 
 const Home: NextPage = () => {
-  const content = [1, 2, 3, 4];
   return <Hero content={content} />;
 };
+
+const content = [
+  <Image src={landscape} alt="whatever" key="1" layout="fill" />,
+  <Image src={stairs} alt="whatever" key="1" layout="fill" />,
+  <Image src={oceanWaves} alt="whatever" key="1" layout="fill" />,
+  <Image src={plantitas} alt="whatever" key="1" layout="fill" />,
+  <Image src={purpleWave} alt="whatever" key="1" layout="fill" />,
+  <Image src={smoothWaves} alt="whatever" key="1" layout="fill" />,
+];
 
 export default Home;
