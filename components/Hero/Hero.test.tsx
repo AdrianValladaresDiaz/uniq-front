c115dd161ae31c5b8a7338e9f3df6d764dc9e6fa
AdrianValladares <a.valladares.d@gmail.com>
2022-04-13 15:01:11 +0200

    tested Hero

--- components/Hero/Hero.test.tsx
+++ components/Hero/Hero.test.tsx
@@ -1,6 +1,7 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import renderWithProviders from "../../mocks/renderWithProviders";
 import Hero from "./Hero";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a Hero component", () => {
   describe("When rendered with a list of contents as a prop", () => {
@@ -12,6 +13,45 @@ describe("Given a Hero component", () => {
       const firstItem = screen.getByText("1");
 
       expect(firstItem).toBeInTheDocument();
+    });
+  });
+
+  describe("When rendered with a content list of length 1", () => {
+    test.todo("Then the hero should not render any buttons");
+  });
+
+  describe("When rendered with buttons", () => {
+    test("Pressing the left or right button should move the hero", async () => {
+      const listOfContent = [1, 2, 3, 4];
+
+      renderWithProviders(<Hero content={listOfContent} />);
+      const leftButton = screen.getByTitle(/move hero left/i);
+      const rightButton = screen.getByTitle(/move hero right/i);
+      const centerContainer = screen.getByText("1");
+      const rightContainer = screen.getByText("2");
+
+      expect(centerContainer.textContent).toBe(`${listOfContent[0]}`);
+      expect(rightContainer.textContent).toBe(`${listOfContent[1]}`);
+
+      await userEvent.click(leftButton);
+
+      await waitFor(
+        () => expect(centerContainer.textContent).toBe(`${listOfContent[3]}`),
+        { timeout: 2000 }
+      );
+
+      expect(centerContainer.textContent).toBe(`${listOfContent[3]}`);
+      expect(rightContainer.textContent).toBe(`${listOfContent[0]}`);
+
+      await userEvent.click(rightButton);
+
+      await waitFor(
+        () => expect(centerContainer.textContent).toBe(`${listOfContent[0]}`),
+        { timeout: 2000 }
+      );
+
+      expect(centerContainer.textContent).toBe(`${listOfContent[0]}`);
+      expect(rightContainer.textContent).toBe(`${listOfContent[1]}`);
     });
   });
 });
